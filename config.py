91c707936e6a61febc8969cdc27024282b8c8764
InfinityEra33 <infinityera33@gmail.com>
2025-12-27 15:16:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,25 +9,21 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "WTF_WhyMeeh")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "ShrutixMusicBot")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "Adityaji4")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "Infinitymusicbot")
 
 MONGO_DB_URI = os.getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", None))
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/NoxxOP/ShrutiMusic")
+UPSTREAM_REPO = os.getenv("UPSTREAM_REPO", "https://github.com/InfinityEra33/Infinity_Music.git")
 UPSTREAM_BRANCH = os.getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = os.getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
-SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/ShrutiBotSupport")
-INSTAGRAM = os.getenv("INSTAGRAM", "https://instagram.com/yaduwanshi_nand")
-YOUTUBE = os.getenv("YOUTUBE", "https://youtube.com/@NandEditz")
-GITHUB = os.getenv("GITHUB", "https://github.com/NoxxOP")
-DONATE = os.getenv("DONATE", "https://t.me/ShrutiBots/91")
-PRIVACY_LINK = os.getenv("PRIVACY_LINK", "https://graph.org/Privacy-Policy-05-01-30")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/infinity_era")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/infinity_era1")
+GITHUB = os.getenv("GITHUB", "https://github.com/InfinityEra33")
 
 DURATION_LIMIT_MIN = int(os.getenv("DURATION_LIMIT", 300))
 PLAYLIST_FETCH_LIMIT = int(os.getenv("PLAYLIST_FETCH_LIMIT", 25))
@@ -47,18 +43,18 @@ STRING5 = os.getenv("STRING_SESSION5", None)
 AUTO_LEAVING_ASSISTANT = bool(os.getenv("AUTO_LEAVING_ASSISTANT", False))
 START_STICKER_ENABLED = bool(os.getenv("START_STICKER_ENABLED", "True").lower() in ["true", "1", "yes"])
 
-START_IMG_URL = os.getenv("START_IMG_URL", "https://files.catbox.moe/7q8bfg.jpg")
-PING_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/eehxb4.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/eehxb4.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/eehxb4.jpg"
+START_IMG_URL = os.getenv("START_IMG_URL", "https://i.ibb.co/GQx3VM1M/x.jpg")
+PING_IMG_URL = "https://i.ibb.co/hR8VsBwd/x.jpg"
+PLAYLIST_IMG_URL = "https://i.ibb.co/7NQmdKVx/x.jpg"
+STATS_IMG_URL = "https://i.ibb.co/KZNNDZ3/x.jpg"
+TELEGRAM_AUDIO_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+TELEGRAM_VIDEO_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+STREAM_IMG_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+SOUNCLOUD_IMG_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+YOUTUBE_IMG_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co/prnzHxFh/x.jpg"
 
 BANNED_USERS = filters.user()
 adminlist = {}
@@ -86,4 +82,4 @@ if SUPPORT_GROUP:
     if not re.match(r"(?:http|https)://", SUPPORT_GROUP):
         raise SystemExit(
             "[ERROR] - SUPPORT_GROUP URL is invalid. It must start with https://"
-        )
+)
